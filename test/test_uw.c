d3d205f2846d3308b1fb1466a01d5bd610c46f0d
petbrain <uebi@tutanota.com>
2025-04-02 11:38:01 +0000

    Moved string tests to the right place.

--- test/test_uw.c
+++ test/test_uw.c
@@ -655,6 +655,40 @@ void test_string()
         TEST(_uw_string_length(&str) == 2500);
         //uw_dump(stderr, &str);
     }
+
+    { // test startswith/endswith
+        UwValue str = uw_create_string("hello world");
+        TEST(uw_startswith(&str, 'h'));
+        TEST(!uw_startswith(&str, U'ค'));
+        TEST(uw_startswith(&str, "hello"));
+        TEST(!uw_startswith(&str, "world"));
+        TEST(uw_endswith(&str, 'd'));
+        TEST(!uw_endswith(&str, U'า'));
+        TEST(uw_endswith(&str, "world"));
+        TEST(!uw_endswith(&str, "hello"));
+    }
+    {
+        UwValue str = uw_create_string(u8"ความคืบหน้า");
+        TEST(!uw_startswith(&str, 'h'));
+        TEST(uw_startswith(&str, U'ค'));
+        TEST(uw_startswith(&str, u8"ความ"));
+        TEST(uw_startswith(&str, U"ความ"));
+        TEST(!uw_startswith(&str, "wow"));
+        TEST(!uw_endswith(&str, 'd'));
+        TEST(uw_endswith(&str, U'า'));
+        TEST(uw_endswith(&str, u8"คืบหน้า"));
+        TEST(uw_endswith(&str, U"คืบหน้า"));
+        TEST(!uw_endswith(&str, "wow"));
+    }
+
+    { // test isdigit
+        UwValue empty = UwString();
+        UwValue nondigit = uw_create_string(u8"123รูปโป๊");
+        UwValue digit = uw_create_string("456");
+        TEST(!uw_string_isdigit(&empty));
+        TEST(!uw_string_isdigit(&nondigit));
+        TEST(uw_string_isdigit(&digit));
+    }
 }
 
 void test_array()
@@ -754,40 +788,6 @@ void test_array()
         UwValue v = uw_array_join(',', &array);
         TEST(uw_equal(&v, " first line,second line,  third line"));
         //uw_dump(stderr, &v);
-    }
-
-    { // test startswith/endswith
-        UwValue str = uw_create_string("hello world");
-        TEST(uw_startswith(&str, 'h'));
-        TEST(!uw_startswith(&str, U'ค'));
-        TEST(uw_startswith(&str, "hello"));
-        TEST(!uw_startswith(&str, "world"));
-        TEST(uw_endswith(&str, 'd'));
-        TEST(!uw_endswith(&str, U'า'));
-        TEST(uw_endswith(&str, "world"));
-        TEST(!uw_endswith(&str, "hello"));
-    }
-    {
-        UwValue str = uw_create_string(u8"ความคืบหน้า");
-        TEST(!uw_startswith(&str, 'h'));
-        TEST(uw_startswith(&str, U'ค'));
-        TEST(uw_startswith(&str, u8"ความ"));
-        TEST(uw_startswith(&str, U"ความ"));
-        TEST(!uw_startswith(&str, "wow"));
-        TEST(!uw_endswith(&str, 'd'));
-        TEST(uw_endswith(&str, U'า'));
-        TEST(uw_endswith(&str, u8"คืบหน้า"));
-        TEST(uw_endswith(&str, U"คืบหน้า"));
-        TEST(!uw_endswith(&str, "wow"));
-    }
-
-    { // test isdigit
-        UwValue empty = UwString();
-        UwValue nondigit = uw_create_string(u8"123รูปโป๊");
-        UwValue digit = uw_create_string("456");
-        TEST(!uw_string_isdigit(&empty));
-        TEST(!uw_string_isdigit(&nondigit));
-        TEST(uw_string_isdigit(&digit));
     }
 }
 
